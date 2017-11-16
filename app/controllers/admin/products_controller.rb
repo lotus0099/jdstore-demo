c6fe7dad2c165dc0cb680d8c252894c5002530f3
lotus0099 <lvdeshi2011@gmail.com>
2017-11-16 12:51:07 +0800

    add admin access tovisit

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,13 +1,20 @@
 class Admin::ProductsController < ApplicationController
+    layout "admin"
+    before_action :authenticate_user!, only: %i[new edit update detroy]
+    before_action :admin_required
+    
     def index
         @products = Product.all
     end
-    def  show
+
+    def show
         @product = Product.find(params[:id])
     end
+
     def new
         @product = Product.new
     end
+
     def create
         @product = Product.new(product_params)
         if @product.save
@@ -16,9 +23,11 @@ class Admin::ProductsController < ApplicationController
             render :new
         end
     end
+
     def edit
         @product = Product.find(params[:id])
     end
+
     def update
         @product = Product.find(params[:id])
         if @product.update(product_params)
@@ -27,14 +36,20 @@ class Admin::ProductsController < ApplicationController
             render :new
         end
     end
+
     def destroy
         @product = Product.find(params[:id])
         @product.destroy
-            redirect_to admin_products_path
+        redirect_to admin_products_path
     end
+
+    def admin_required
+        redirect_to '/', alert: 'You are not admin.' unless current_user.admin?
+end
+
     private
 
     def product_params
-        params.require(:product).permit(:title,:description,:price,:quantity)
+        params.require(:product).permit(:title, :description, :price, :quantity)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
     namespace :admin do
         resources :products
     end
-    root 'admin/products#index'
+    root 'welcome#index'
 end
