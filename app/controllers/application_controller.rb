b0e6d79b4b0b62fc6b8400c5a265f2a8377c1939
lotus0099 <lvdeshi2011@gmail.com>
2017-11-16 23:12:26 +0800

    implement current_cart function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+    protect_from_forgery with: :exception
+
+    helper_method :current_cart
+    def current_cart
+        @current_cart ||= find_cart
+    end
+
+    def find_cart
+        cart = Cart.find_by(id: session[:cart_id])
+
+        cart = Cart.create if cart.blank?
+        session[:cart_id] = cart.id
+        cart
+    end
 end
