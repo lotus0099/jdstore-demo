e975e2a98066791cee5724e8cf60a186fbc35bc1
lotus0099 <lvdeshi2011@gmail.com>
2017-11-17 22:36:30 +0800

    implement cart_total_price function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,15 +2,20 @@ class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
 
     helper_method :current_cart
+
     def current_cart
-        @current_cart ||= find_cart
+      @current_cart ||= find_cart
     end
+
+
+    private
 
     def find_cart
         cart = Cart.find_by(id: session[:cart_id])
-
-        cart = Cart.create if cart.blank?
+        if cart.blank?
+          cart = Cart.create
+        end
         session[:cart_id] = cart.id
-        cart
+        return cart
     end
 end
