14275a57e4c9e7edcb1a4f91f46c7c356573e107
lotus0099 <lvdeshi2011@gmail.com>
2017-11-18 12:34:49 +0800

    empty cart

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -10,12 +10,15 @@ class Cart < ApplicationRecord
     end
 
     def total_price
-    sum = 0
-    cart_items.each do |cart_item|
-      if cart_item.product.price.present?
-        sum += cart_item.quantity * cart_item.product.price
-      end
+        sum = 0
+        cart_items.each do |cart_item|
+        if cart_item.product.price.present?
+            sum += cart_item.quantity * cart_item.product.price
+        end
+        end
+        sum
     end
-    sum
-  end
+    def clean!
+        cart_items.destroy_all
+    end
 end
